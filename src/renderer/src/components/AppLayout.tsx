9158af0b1e2bef674d3cbac77a0e47ec4fa4042a
Ubaton <raymondNG729@gmail.com>
2024-02-24 11:42:19 +0200

    v0.0.2

---
+++ src/renderer/src/components/AppLayout.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps, Ref, forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export const RootLayout = ({ children, className, ...props }): ComponentProps<'main'> => {
+  return (
+    <main className={twMerge('flex flex-row h-screen', className)} {...props}>
+      {children}
+    </main>
+  )
+}
+
+export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
+  return (
+    <aside
+      className={twMerge('w-[205px] mt-10 h-[100vh + 10px] overflow-auto', className)}
+      {...props}
+    >
+      {children}
+    </aside>
+  )
+}
+
+export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ children, className, ...props }, ref: Ref<HTMLDivElement>) => {
+    return (
+      <div ref={ref} className={twMerge('flex-1 overflow-auto', className)} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
+
+Content.displayName = 'Content'
