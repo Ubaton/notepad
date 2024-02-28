6cd394a45ac662955a81a17342394888e40da8da
Ubaton <raymondNG729@gmail.com>
2024-02-28 22:57:20 +0200

    v0.0.7

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,7 @@ import {
   ActionsButtonsRow,
   Content,
   DraggableTopBar,
+  MarkdownEditor,
   NotePreviewList,
   RootLayout,
   Sidebar
@@ -16,7 +17,9 @@ const App = () => {
           <ActionsButtonsRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border bg-zinc-900/50 border-l-white/20">
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
