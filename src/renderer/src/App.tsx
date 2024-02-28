3b927f2dc56d778b4cf465359252df0f5a2568dd
Ubaton <raymondNG729@gmail.com>
2024-02-28 17:49:20 +0200

    v0.0.6

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,20 @@
-import { ActionsButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
+import {
+  ActionsButtonsRow,
+  Content,
+  DraggableTopBar,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from '@/components'
 
 const App = () => {
   return (
     <>
       <DraggableTopBar />
       <RootLayout className={'root-layout'}>
-        <Sidebar className="flex justify-between mt-1">
-          <ActionsButtonsRow />
+        <Sidebar className="p-2">
+          <ActionsButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
