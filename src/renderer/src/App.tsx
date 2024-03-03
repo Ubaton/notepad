5b76664af0ffec6bf3791d30885b92f912c8e4a6
Ubaton <raymondNG729@gmail.com>
2024-03-03 22:09:42 +0200

    v0.0.10

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,17 +8,24 @@ import {
   RootLayout,
   Sidebar
 } from '@/components'
+import { useRef } from 'react'
 
 const App = () => {
+  const contentContainRef = useRef<HTMLDivElement>(null)
+
+  const resetScroll = () => {
+    contentContainRef.current?.scrollTo(0, 0)
+  }
+
   return (
     <>
       <DraggableTopBar />
       <RootLayout className={'root-layout'}>
         <Sidebar className="p-2">
           <ActionsButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 space-y-1" />
+          <NotePreviewList onSelect={resetScroll} className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border bg-zinc-900/50 border-l-white/20">
+        <Content ref={contentContainRef} className="border bg-zinc-900/50 border-l-white/20">
           <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
